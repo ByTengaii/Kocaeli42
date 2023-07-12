d18a0c5d813fad80673f55e33418522cbd2ba04e
Bytengaii <agirgurkan06@gmail.com>
2023-07-12 19:01:46 +0300

    ex06 is done

---
+++ c_00/ex06/ft_print_comb2.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+
+void ft_print_comb2(void) {
+    char buffer[7];
+    int i, j;
+
+    for (i = 0; i <= 98; i++) {
+        for (j = i + 1; j <= 99; j++) {
+            buffer[0] = '0' + (i / 10);// tens digit
+			buffer[1] = '0' + (i % 10); // ones digit
+            buffer[2] = ' ';
+            buffer[3] = '0' + (j / 10); //tens digit
+            buffer[4] = '0' + (j % 10); // ones digit
+            buffer[5] = ',';
+            buffer[6] = ' ';
+
+			if(i == 98){
+				write(1,buffer,5);
+			}else{
+			 	write(1, buffer, 7); 
+			}
+        }
+    }
+}
+
