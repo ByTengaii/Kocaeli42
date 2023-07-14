4ffbb03bfe3fb777f96cf39413c2d6b894d395d9
Bytengaii <agirgurkan06@gmail.com>
2023-07-14 11:43:02 +0300

    c_00/ex06 norminette'ye göre düzenlendi. Fonksiyon 2'ye bölündü. Pointerlı fonksyion var.

--- c_00/ex06/ft_print_comb2.c
+++ c_00/ex06/ft_print_comb2.c
@@ -6,44 +6,45 @@
 /*   By: gagir <gagir@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:08:11 by gagir             #+#    #+#             */
-/*   Updated: 2023/07/14 11:12:39 by gagir            ###   ########.fr       */
+/*   Updated: 2023/07/14 11:42:07 by gagir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_print_comb2( void ) 
+void	write_to_buffer(char *buffer, int i, int j)
 {
-    char	buffer[7];
-    int	i;
-	int	j;
-	
-	i=0;
-	while(i <= 98)
+	buffer[0] = '0' + (i / 10);
+	buffer[1] = '0' + (i % 10);
+	buffer[2] = ' ';
+	buffer[3] = '0' + (j / 10);
+	buffer[4] = '0' + (j % 10);
+	buffer[5] = ',';
+	buffer[6] = ' ';
+	if (i == 98)
+	{
+		write(1, buffer, 5);
+	}
+	else
+	{
+		write(1, buffer, 7); 
+	}
+}
+
+void	ft_print_comb2(void)
+{
+	char	buffer[7];
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i <= 98)
 	{
 		j = i + 1;
-		while(j <= 99)
-		{	
-			buffer[0] = '0' + (i / 10);// tens digit
-			buffer[1] = '0' + (i % 10); // ones digit
-            buffer[2] = ' ';
-            buffer[3] = '0' + (j / 10); //tens digit
-            buffer[4] = '0' + (j % 10); // ones digit
-            buffer[5] = ',';
-            buffer[6] = ' ';
-
-			if(i == 98){
-				write(1,buffer,5);
-			}else{
-			 	write(1, buffer, 7); 
-			}
+		while (j <= 99)
+		{
+			write_to_buffer(buffer, i, j);
 			j++;
 		}
 		i++;
 	}
-}
-
-int	main()
-{
-	ft_print_comb2();
 }
