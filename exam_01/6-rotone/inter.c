698421396ec03a1e2871de9878a25a536c7e958b
Gurkan Agir <gagir@k1m17s02.42kocaeli.com.tr>
2023-07-23 11:09:25 +0300

    exam_01 is updated

--- exam_01/6-rotone/inter.c
+++ exam_01/6-rotone/inter.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
-void	putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
 
-int	strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int		i;
 
@@ -24,6 +24,7 @@ int	checkLog(char *log, char c)
 	{
 		if (log[i] == c)
 			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -38,20 +39,27 @@ void	inter(char *str1, char *str2)
 	i = 0;
 	j = 0;
 	log_index = 0;
-	size = strlen(str1);
+	size = ft_strlen(str1);
 	
 	char 	log[size];
 	
 	while (str1[i] != '\0')
 	{
 		j = 0;
-		if (!checkLog(log, str[i]))
+		if (!checkLog(log, str1[i]))
 		{
 			while (str2[j] != '\0')
 			{
 				if (str1[i] == str2[j])
+				{
+					ft_putchar(str1[i]);
+					log[log_index++] = str1[i];
+					break;
+				}
+				j++;
 			}
-		}	
+		}
+		i++;	
 	}
 }
 
@@ -59,5 +67,6 @@ int	main(int ac, char **av)
 {
 	if (ac == 3)
 		inter(av[1], av[2]);
-
+	write(1, "\n",1);
+	return (0);
 }
