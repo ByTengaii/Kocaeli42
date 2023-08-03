08513cba4445b4e6a41baae68a1f855296ddfb27
Gurkan Agir <gagir@k1m15s02.42kocaeli.com.tr>
2023-08-03 12:03:27 +0300

    last

--- c_05/ex04/ft_fibonacci.c
+++ c_05/ex04/ft_fibonacci.c
@@ -6,23 +6,17 @@
 /*   By: gagir <gagir@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 14:38:38 by gagir             #+#    #+#             */
-/*   Updated: 2023/08/02 17:03:34 by gagir            ###   ########.fr       */
+/*   Updated: 2023/08/03 11:56:55 by gagir            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_fibonacci(int index)
 {
-	int	aux;
-
+	if (index < 0)
+		return (-1);
 	if (index == 0)
 		return (0);
-	else if (index == 1)
-		return (1);
-	else if (index > 1)
-	{
-		aux = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
-		return (aux);
-	}
-	else
-		return (-1);
+	if (index == 1)
+		return (1); 
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
 }
---
+++ exam_02/6-ft_split/ft_split.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+
+int	count_word(char *str)
+{
+	int in_word = 1;
+	int count = 0;
+	while(*str)
+	{
+		if(*str == ' ' || *str == '\n' || *str == '\t')
+			in_word = 1;
+		else if(in_word)
+		{
+			count++;
+			in_word = 0;
+		}
+		str++;
+	}
+	return (count);
+}
+
+char *get_next_word(char **str_ptr)
+{
+	char *start = *str_ptr;
+	while(**str_ptr && !(**str_ptr == ' ' || **str_ptr == '\t' || **str_ptr == '\n'))
+		(*str_ptr)++;
+	int len = *str_ptr - start;
+	char *word = (char*)malloc(len+1);
+	if(word)
+	{
+		for(int i =  0; i < len; i++)
+		{
+			word[i] = start[i];
+		}
+		word[len] = '\0';
+		return (word);
+	}
+	else
+		return NULL;
+}
+
+char **ft_split(char *str)
+{
+	int words = count_word(str);
+
+	char **arr = (char*)malloc(sizeof(char*) + (words+1));
+	if(arr)
+	{
+		for (int i = 0 ; i < words ; i++)
+		{
+			while(*str == ' ' || *str == '\t' || *str == '\n')
+				str++;
+			arr[i] = get_next_word(&str);
+			if(!arr[i])
+			{
+				for(int j =  0; j < i ; j++)
+				{
+
+				}
+			}
+		}
+	}
+	else
+		return NULL;
+}
